f4500e6c2299d3cf190c179304025108aeb22d19
Noureddine <en_metourni@esi.dz>
2017-03-28 16:14:05 +0100

    new commit

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,12 @@
  */
 
 $('.img-circle').on('click',function () {
+    var url = $(this).attr('src');
     swal({
-        title: "HTML <small>Title</small>!",
-        text: "A custom <span style=\"color:#F8BB86\">html<span> message.",
+        title: "HTML <small>Title </small>!",
+        text: "A custom <span style=\"color:#F8BB86\">html<span> message. " +
+        "<br>" +
+        "<img class=\"img-circle\" src="+url+"/>",
         html: true
     }) ;
 });
