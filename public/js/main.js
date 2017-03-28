f59f2689f6e585d3aba6f3b2092d99324b19a971
Noureddine <en_metourni@esi.dz>
2017-03-28 16:06:51 +0100

    new commit

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,11 @@
 /**
  * Created by Noureddine Metourni on 26/03/2017.
  */
+
+$('.img-circle').on('click',function () {
+    swal({
+        title: "HTML <small>Title</small>!",
+        text: "A custom <span style=\"color:#F8BB86\">html<span> message.",
+        html: true
+    }) ;
+});
